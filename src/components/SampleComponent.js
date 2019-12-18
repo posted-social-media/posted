d2db2e05458dd178409bac5f67994872b3c864ab
Vinni Hoke <vinni.hoke@gmail.com>
2019-12-17 19:11:03 -0800

    Completed Boilerplate

---
+++ src/components/SampleComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../resources/img/posted-logo-full-white.png';
+
+const SampleComponent = () => {
+	return (
+		<div>
+			<img src={Logo} alt="" />
+		</div>
+	)
+}
+
+export default SampleComponent
---
+++ src/views/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SampleComponent from "../components/SampleComponent";
+
+const Profile = () => {
+  return (
+    <div>
+      <SampleComponent />
+      <h5>This is where components will live</h5>
+    </div>
+  );
+};
+
+export default Profile;
