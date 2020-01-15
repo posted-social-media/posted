0ccd923c4d52277b837d269e62630c542dfa9b56
Jose <Jrivera6869@yahoo.com>
2020-01-15 13:47:51 -0500

    changed file locations

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // React/Components
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 // UI
-import './scss/index.scss';
-import 'bootstrap/dist/css/bootstrap.css';
-import * as serviceWorker from './serviceWorker';
+import "./theme/scss/index.scss";
+import "bootstrap/dist/css/bootstrap.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
